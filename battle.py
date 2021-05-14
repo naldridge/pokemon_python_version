ae4e805a26b9fa598bf27fd6b31a7d9d5d9db6ef
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-05-14 12:19:47 -0400

    updated battle and further organized classes

--- battle.py
+++ battle.py
@@ -14,28 +14,40 @@ class Battle:
             print(f'{self.enemy.name} sent out {self.enemy.pokemon_party[0].name}!')
             print("")
             print(f'{self.trainer.name} sent out {self.trainer.pokemon_party[0].name}!')
-            # self.alive(self.trainer.pokemon_party)
-            while len(self.trainer.pokemon_party) > 0 and len(self.enemy.pokemon_party) > 0:
+            # self.party_status(self.trainer.pokemon_party)
+            while self.party_status(self.trainer.pokemon_party) > 0 and self.party_status(self.enemy.pokemon_party) > 0:
                 
                 
                 # substitute all 0's with new variables to signify the current index
 
-                while self.trainer.pokemon_party[0].health > 0 and self.enemy.pokemon_party[0].health > 0:
-                    # print("What would you like to do? ")
+                while self.trainer.pokemon_party[current_pokemon].health > 0 and self.enemy.pokemon_party[enemy_pokemon].health > 0:
+                    # Pokemon attack options for user
                     t_party = self.trainer.pokemon_party
                     e_party = self.enemy.pokemon_party
                     count = 0
-                    while len(t_party[0].attacks) > count:
-                        print(f'{count + 1}. {t_party[0].attacks[count].name}')
+                    while len(t_party[current_pokemon].attacks) > count:
+                        print(f'{count + 1}. {t_party[current_pokemon].attacks[count].name}')
                         count += 1
-                    user_input = Validate().range(1, len(t_party[0].attacks), "Please choose a valid option", "What would you like to do? ")
+                    user_input = Validate().range(1, len(t_party[current_pokemon].attacks), "Please choose a valid option", "What would you like to do? ")
                     #user_input = input()
-                    print(f'You chose {t_party[0].attacks[user_input - 1].name}')
-                    e_party[0].health -= t_party[0].attacks[user_input - 1].damage
-                    print(f'The enemy {e_party[0].name} now has {e_party[0].health} HP')
+                    print(f'You chose {t_party[current_pokemon].attacks[user_input - 1].name}')
+                    e_party[enemy_pokemon].health -= t_party[current_pokemon].attacks[user_input - 1].damage
+                    #t_party[current_pokemon].health -= e_party[current_pokemon].attacks[0].damage
+                    print(f'The enemy {e_party[enemy_pokemon].name} now has {e_party[enemy_pokemon].health} HP')
+                    if self.alive(self.enemy.pokemon_party[enemy_pokemon]):
+                        t_party[current_pokemon].health -= e_party[current_pokemon].attacks[0].damage
+                        print(f'The enemy {e_party[enemy_pokemon].name} used {e_party[enemy_pokemon].attacks[0].name}, {t_party[current_pokemon].name} has {t_party[current_pokemon].health}')
+
+                
+                if self.alive(self.trainer.pokemon_party[current_pokemon]):
+                    print(f'Enemy {self.enemy.pokemon_party[enemy_pokemon].name} fainted!')
+                    enemy_pokemon += 1
+                else:
+                    print(f'Your {self.trainer.pokemon_party[current_pokemon].name} fainted!')
+                    current_pokemon += 1
                 # call new single alive function
                 # This is where you check whose pokemon fainted, add one to the variable and it will restart the loop
-        if self.alive(self.trainer.pokemon_party):
+        if self.party_status(self.trainer.pokemon_party):
             print("You won")
         else:
             print("You lost")
@@ -43,7 +55,7 @@ class Battle:
 
 
 # make a new function to check alive status
-    def alive(self, party):
+    def party_status(self, party):
         count = 0
         dead_pokemon = 0
         while len(party) > count:
@@ -55,3 +67,9 @@ class Battle:
         else:
             return True
 # whenever dead pokemon variable is equal to the length of your pokemon party, return false
+
+    def alive(self, pokemon):
+        if pokemon.health > 0:
+            return True
+        else:
+            return False
--- pokemon_class.py
+++ pokemon_class.py
@@ -1,3 +1,7 @@
+from trainer_class import Trainer
+from trainer_class import Enemy
+from battle import Battle
+
 class Pokemon:
     def __init__(self, name, health, attacks):
         self.name = name
@@ -8,3 +12,44 @@ class Attack:
     def __init__(self, name, damage):
         self.name = name
         self.damage = damage
+
+# # Creating Pokemon Attacks
+fire_blast = Attack("Fire Blast", 50)
+wing_attack = Attack("Wing Attack", 30)
+slash = Attack("Slash", 30)
+flamethrower = Attack("Flamethrower", 40)
+crunch = Attack("Crunch", 35)
+feint_attack = Attack("Feint Attack", 30)
+thunder = Attack("Thunder", 50)
+thunder_bolt = Attack("Thunder Bolt", 35)
+
+houndoom_attacks = [fire_blast, crunch, feint_attack, flamethrower]
+electabuzz_attacks = [thunder, thunder_bolt, slash, wing_attack]
+snorlax_attacks = [feint_attack, slash, flamethrower, thunder]
+charizard_attacks = [flamethrower, wing_attack, slash, fire_blast]
+
+
+houndoom = Pokemon("Houndoom", 110, houndoom_attacks )
+electabuzz = Pokemon("Electabuzz", 110, electabuzz_attacks)
+snorlax = Pokemon("Snorlax", 200, snorlax_attacks)
+charizard = Pokemon("Charizard", 110, )
+
+sampai = Enemy("Sam-pai", [houndoom, snorlax])
+trainer = Trainer("Rodrigo", [electabuzz, charizard])
+
+
+print("""
+______     _                               ______      _   _                   _   _               _             
+| ___ \   | |                              | ___ \    | | | |                 | | | |             (_)            
+| |_/ /__ | | _____ _ __ ___   ___  _ __   | |_/ /   _| |_| |__   ___  _ __   | | | | ___ _ __ ___ _  ___  _ __  
+|  __/ _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \  |  __/ | | | __| '_ \ / _ \| '_ \  | | | |/ _ \ '__/ __| |/ _ \| '_ \ 
+| | | (_) |   <  __/ | | | | | (_) | | | | | |  | |_| | |_| | | | (_) | | | | \ \_/ /  __/ |  \__ \ | (_) | | | |
+\_|  \___/|_|\_\___|_| |_| |_|\___/|_| |_| \_|   \__, |\__|_| |_|\___/|_| |_|  \___/ \___|_|  |___/_|\___/|_| |_|
+                                                  __/ |                                                          
+                                                 |___/                                                           
+""")
+input("Press enter to continue")
+print("\033c")
+
+battle1 = Battle(trainer, sampai)
+battle1.battle()
