c853cfe2230c67436180b676e47d7573071a4231
Miguel Folgar <miguelangelfolgar@gmail.com>
2021-05-16 19:14:54 -0500

    Sound file

---
+++ sound.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+playsound('Arbok_cry.mp3')
+playsound('gymleader.mp3')
+playsound('Houndoom_cry.mp3')
+playsound('Mewtwo_cry.mp3')
+playsound('Persian_cry.mp3')
+playsound('StartMenutheme.mp3')
+playsound('Teamrocket_Hideout.mp3')
